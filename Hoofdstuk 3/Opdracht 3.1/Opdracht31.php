6e9b6cebf9495892d72f311b8ca5aaad758a75fd
Anil Korkmaz <anil.korkmaz@edu-kw1c.nl>
2020-02-21 10:17:19 +0100

    Opdracht31.php taak 1

--- Hoofdstuk 3/Opdracht 3.1/Opdracht31.php
+++ Hoofdstuk 3/Opdracht 3.1/Opdracht31.php
@@ -5,24 +5,31 @@ include '../../Includes/sidemenu.php';
 
     <?php
         $event = " Elfstedentocht "; //String
-        $fries = " Alvestêdetocht "; //String
+        $fries = " Alvestêdetocht"; //String
         $distance = 200;             //Integer
         $sport = " schaatstoch ";    //String
         $kind = " natuurijs ";       //String
-        $organizer = " Koninklijke Vereniging De Friesche Elf Steden "; //String
-        $land1 = " Leeuwarden";      //String
-        $land2 = " Friesland";       //String
+        $organizer = " Koninklijke Vereniging De Friesche Elf Steden"; //String
+        $capital = " Leeuwarden";      //String
+        $land = " Friesland";       //String
         $timesRode = 15;                 //Integer
         $year = 1909;                //Integer
         $timesHeld = 1;              //Integer
 
-        $verhaal = "De" . $event . "(Fries:" . $fries . ")"
+        $verhaal = "De" . $event . "(Fries:" . $fries . ") is een " . $distance . "kilometer"
+        . "lange " . $sport . "over" . $kind . " die wordt georganiseerd door"
+        . "de" . $organizer . ". " . $capital . ","
+        . " de hoofdstad van " . $land . ", is start- en aankomstplaats. De"
+        . $event . " is inmiddels " . $timesRode . " maal verreden en werd voor het"
+        . "eerst in " . $year . " gereden en wordt maximaal " . $timesHeld . " keer per winter gehouden.";
     ?>
 
-<main id="wrapper">
-    <h2>Uitwerkingen</h2>
-
-</main>
-</body>
+        <main id="wrapper">
+            <h2>Uitwerkingen</h2>
+            <?php
+                echo "<p>" . $verhaal . "</p>";
+            ?>
+        </main>
+    </body>
 </html>
 
