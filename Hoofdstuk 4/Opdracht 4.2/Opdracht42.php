fb3c1dfc57ffd014c1c6526649ab9cc331afe0d3
Anil Korkmaz <anil.korkmaz@edu-kw1c.nl>
2020-03-05 20:58:28 +0100

    Add Opdracht42.php, script.php

---
+++ Hoofdstuk 4/Opdracht 4.2/Opdracht42.php
@@ -0,0 +1,15 @@
+<?php
+include '../../Includes/header.php';
+include '../../Includes/sidemenu.php';
+include '../../Includes/footer.php';
+include '../../Includes/script.php';
+?>
+        <main id="wrapper">
+            <h2>Uitwerkingen</h2>
+                <?php
+                    echo "<p>" . " Voor het vak " . "<span class='bold'>" . $courseName . "</span>" . " heb je " . "<span class='bold'>" . $teacherName . "</span>" . " als docent." . "</p>"
+                ?>
+        </main>
+    </body>
+</html>
+
---
+++ Includes/script.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * PHP - Remco Evers
+ * ASP - Ralph Gijsbrechts
+ * Javascript - Martijn van de Wetering
+ * Modelleren - Ralph Gijsbrechts
+ * Engels - Sandra Mitrovic
+ * Nederlands - Jan van Rijswijk
+ * Rekenen - Martijn van Meer
+ * Digitale vaardigheden - Larissa Pielage
+ * SQL - Martijn van de Wetering
+ * Computertekenen - Pieter van den Berg
+ * SLB - Angela Lambregts
+ * L&B - Angela Lambregts
+ */
+
+$courseName = "Javascript";
+$teacherName = "Remco Evers";
+
+switch ($courseName)
+{
+    case "PHP":
+        $teacherName;
+        break;
+    case "ASP":
+        $teacherName = "Ralph Gijsbrechts";
+        break;
+    case "Javascript":
+        $teacherName = "Martijn van de Wetering";
+        break;
+    case "Modelleren":
+        $teacherName = "Ralph Gijsbrechts";
+        break;
+    case "Engels":
+        $teacherName = "Sandra Mitrovic";
+        break;
+    case "Nederlands":
+        $teacherName = "Jan van Rijswijk";
+        break;
+    case "Rekenen":
+        $teacherName = "Martijn van Meer";
+        break;
+    case "Digitale vaardigheden":
+        $teacherName = "Larissa Pielage";
+        break;
+    case "SQL":
+        $teacherName = "Martijn van de Wetering";
+        break;
+    case "Computertekenen":
+        $teacherName = "Pieter van den Berg";
+        break;
+    case "SLB":
+        $teacherName = "Angela Lambregts";
+        break;
+    case "L&B":
+        $teacherName = "Angela Lambregts";
+        break;
+}
